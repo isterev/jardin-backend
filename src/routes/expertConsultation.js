83faae9b6273e56335e06183919f6444614ba341
Srajit Sakhuja <Srajit.Sakhuja@kpit.com>
2020-07-19 23:21:50 +0200

    add middleware to expert consultation

--- src/routes/expertConsultation.js
+++ src/routes/expertConsultation.js
@@ -3,13 +3,14 @@
 const express  = require('express');
 const router   = express.Router();
 const ExpertConsultationController = require('../controllers/expertConsultation')
+const checkAuthentication = require("../middlewares").checkAuthentication;
 
-router.post('/consultation-requests', ExpertConsultationController.createRequest)
-router.put('/consultation-requests', ExpertConsultationController.updateRequest)
-router.get('/consultation-requests', ExpertConsultationController.getRequests)
-router.get('/consultation-requests/:id', ExpertConsultationController.getConsultationSessionById)
-router.get('/consultation-sessions', ExpertConsultationController.getConsultationSessions)
-router.get('/consultation-sessions/:id', ExpertConsultationController.getConsultationSessionById)
-router.post('/experts', ExpertConsultationController.createExpert)
-router.get('/experts', ExpertConsultationController.getExperts)
+router.post('/consultation-requests', checkAuthentication, ExpertConsultationController.createRequest)
+router.put('/consultation-requests', checkAuthentication,ExpertConsultationController.updateRequest)
+router.get('/consultation-requests', checkAuthentication,ExpertConsultationController.getRequests)
+router.get('/consultation-requests/:id', checkAuthentication,ExpertConsultationController.getConsultationSessionById)
+router.get('/consultation-sessions', checkAuthentication,ExpertConsultationController.getConsultationSessions)
+router.get('/consultation-sessions/:id', checkAuthentication,ExpertConsultationController.getConsultationSessionById)
+router.post('/experts', checkAuthentication,ExpertConsultationController.createExpert)
+router.get('/experts', checkAuthentication, ExpertConsultationController.getExperts)
 module.exports = router
